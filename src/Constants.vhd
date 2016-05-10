66404e89a5fa24c48a6e611a33ff2b460bb5f9fc
Raman But-Husaim <raman_buthusaim@yahoo.com>
2016-05-10 09:17:59 +0300

    [Accumulator]: Forgot to modify the last file.

--- src/Constants.vhd
+++ src/Constants.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 package Sorting_Design is
-	-- OP Codes
-	constant OP_LOAD: std_logic_vector(3 downto 0) := "0000";
-	constant OP_STORE: std_logic_vector(3 downto 0) := "0001";
-	constant OP_ADD: std_logic_vector(3 downto 0) := "0010";
-	constant OP_SUB: std_logic_vector(3 downto 0) := "0011";
-	constant OP_HALT: std_logic_vector(3 downto 0) := "0100";
+    -- OP Codes
+    constant OP_LOAD: std_logic_vector(3 downto 0) := "0000";
+    constant OP_STORE: std_logic_vector(3 downto 0) := "0001";
+    constant OP_ADD: std_logic_vector(3 downto 0) := "0010";
+    constant OP_SUB: std_logic_vector(3 downto 0) := "0011";
+    constant OP_HALT: std_logic_vector(3 downto 0) := "0100";
     constant OP_LOADIN: std_logic_vector(3 downto 0) := "0101";
-	constant OP_JNSB: std_logic_vector(3 downto 0) := "0110";
-	constant OP_JZ: std_logic_vector(3 downto 0) := "0111";
-	constant OP_STOREIN: std_logic_vector(3 downto 0) := "1000";
-	-- End OP Codes
+    constant OP_JNSB: std_logic_vector(3 downto 0) := "0110";
+    constant OP_JZ: std_logic_vector(3 downto 0) := "0111";
+    constant OP_STOREIN: std_logic_vector(3 downto 0) := "1000";
+    -- End OP Codes
 end Sorting_Design;
